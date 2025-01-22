433ee8562371f77f326f69217e86134039a0e46e
kushal bhargava <kushal.git01@gmail.com>
2025-01-22 15:25:28 +0530

    About page updated

---
+++ src/data/socialData.js
@@ -0,0 +1,24 @@
+const socialData = [
+  {
+    name: "linkedin",
+    href: "https://www.linkedin.com/in/kushal-bhargava/",
+    icon: "tabler:brand-linkedin",
+  },
+  {
+    name: "github",
+    href: "https://github.com/haider0107/",
+    icon: "tabler:brand-github",
+  },
+  {
+    name: "twitter",
+    href: "https://x.com/haider0107",
+    icon: "tabler:brand-twitter",
+  },
+  {
+    name: "blog",
+    href: "/blog/1",
+    icon: "tabler:brand-blogger",
+  },
+];
+
+export default socialData;
