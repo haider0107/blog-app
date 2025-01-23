ca8831c90cf7eb58f3f4dabf8d908e1ba4d468fb
kushal bhargava <kushal.git01@gmail.com>
2025-01-23 22:56:07 +0530

    About page updated

---
+++ src/data/projectData.js
@@ -0,0 +1,16 @@
+const projectData = [
+  {
+    name: "FeedBack App",
+    link: "https://github.com/haider0107/feedbackapp",
+    image: "/images/feedback-app.png",
+    desc: "",
+  },
+  {
+    name: "Blog App",
+    link: "/",
+    image: "/images/image-default.png",
+    desc: "",
+  },
+];
+
+export default projectData;
---
+++ src/data/skillsData.js
@@ -0,0 +1,25 @@
+const skillsData = {
+  frontend: [
+    { name: "React.js", icon: "skill-icons:react-dark" },
+    { name: "Next.js", icon: "skill-icons:nextjs-dark" },
+    { name: "Astro", icon: "skill-icons:astro" },
+    { name: "Remix", icon: "skill-icons:remix-dark" },
+  ],
+  backend: [
+    { name: "Remix", icon: "skill-icons:remix-dark" },
+    { name: "Express.js", icon: "skill-icons:expressjs-dark" },
+    { name: "ASP.NET", icon: "skill-icons:dotnet" },
+  ],
+  labguage: [
+    { name: "JavaScript", icon: "skill-icons:javascript" },
+    { name: "C#", icon: "skill-icons:cs" },
+    { name: "TypeScript", icon: "skill-icons:typescript" },
+  ],
+  DataBase: [
+    { name: "MongoDB", icon: "skill-icons:mongodb" },
+    { name: "MySQL", icon: "skill-icons:mysql-dark" },
+    { name: "PostgreSQL", icon: "skill-icons:postgresql-dark" },
+  ],
+};
+
+export default skillsData;
